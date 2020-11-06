8681be5442736a080db38df98907ae4f615587b5
Liubomyr <louidropson@gmail.com>
2020-11-06 13:59:44 +0200

    finished 3 tasks

--- tasks/alg_tasks/task_134/assets/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keyup", (e) => {
-  const houseWindow = document.querySelector(".window");
-  if (e.key === "Enter") {
-    houseWindow.classList.toggle("activeWindow");
-  }
-});
---
+++ tasks/alg_tasks/task_562/assets/script.js
@@ -0,0 +1,16 @@
+const getArmstrongNumbers = () => {
+  const arr = [];
+  for (let i = 10; i < 100000; i++) {
+    arr.push(i);
+  }
+  return arr
+    .map((num) => {
+      return num
+        .toString()
+        .split("")
+        .map((el, i, arr) => Math.pow(el, arr.length))
+        .reduce((a, b) => a + b);
+    })
+    .filter((el, i) => el === arr[i]);
+};
+console.log(getArmstrongNumbers());
